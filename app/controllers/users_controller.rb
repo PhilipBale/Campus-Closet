d6a86513ed93780acbd1f974713a97c904f9c03b
Philip Bale <pbale95@gmail.com>
2015-01-08 21:22:57 -0500

    Hooking up login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	def create
+		@user = User.new(user_params)    # Not the final implementation!
+	    if @user.save
+	      flash[:success] = "Succesful Registration"
+	      redirect_to root_path
+	    else
+	      render 'login/login'
+	    end
+	end
+
+	private
+	    def user_params
+	      params.require(:user).permit(:name, :email, :phone, :password,
+	                                   :password_confirmation)
+	    end
 end
