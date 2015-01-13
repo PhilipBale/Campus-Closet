53e2600872e8a4d86f090aeda2ff63497dd14ab7
Philip Bale <pbale95@gmail.com>
2015-01-13 08:30:47 -0500

    Adding basic clothing info/barcode support

--- app/controllers/clothing_controller.rb
+++ app/controllers/clothing_controller.rb
@@ -1,4 +1,9 @@
 class ClothingController < ApplicationController
+  require 'barby'
+  require 'barby/barcode/code_128'
+  require 'barby/outputter/png_outputter'
+  require 'barby/outputter/html_outputter'
+  
   def new
   end
 
@@ -8,5 +13,12 @@ class ClothingController < ApplicationController
   	if @clothing.blank? || @clothing == nil
   		@clothing = Clothing.find_by_id(params[:id])
   	end
+ 	
+ 	if @clothing.present?
+      @barcode = Barby::Code128B.new(@clothing.clothing_code)
+      @barcode_for_html = Barby::HtmlOutputter.new(@barcode)
+      blob = Barby::PngOutputter.new(@barcode).to_png #Raw PNG data
+      File.open('barcode.png', 'w'){|f| f.write blob }
+    end
   end
 end
